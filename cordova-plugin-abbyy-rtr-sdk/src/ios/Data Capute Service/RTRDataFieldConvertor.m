8b529b97aeac2d4a745b41295ab85fc7255ffa4c
Ostap Kyselytsia <ostap.kyselytsia@startupsoft.co>
2019-11-08 15:51:00 +0200

    tested functionality. Improved data convertor.

--- cordova-plugin-abbyy-rtr-sdk/src/ios/Data Capute Service/RTRDataFieldConvertor.m
+++ cordova-plugin-abbyy-rtr-sdk/src/ios/Data Capute Service/RTRDataFieldConvertor.m
@@ -18,7 +18,7 @@
 
 -(NSDictionary*) convertToDictionary: (NSArray<RTRDataField*>*)dataFields {
     NSDictionary* finalDictionary = @{
-        @"dataFields" : [self convertDataFieldToArrayMap: dataFields],
+        @"dataFields" : [self arrayFromDataFields: dataFields],
         @"dataScheme": @{ @"id": @"BusinessCards", @"name": @"Business Cards" }
     };
     
@@ -27,47 +27,28 @@
 
 // MARK: - Private
 
--(NSArray*) convertDataFieldToArrayMap: (NSArray<RTRDataField*>*)dataFields {
-    NSMutableArray<NSDictionary*>* array = [NSMutableArray new];
-    
-    for (RTRDataField* data in dataFields) {
-        NSMutableDictionary* object = [self rtrDataFieldObject: data];
-        // components
-        if (data.components != nil) {
-            NSMutableArray<NSDictionary*>* subArray = [NSMutableArray new];
-            
-            for (RTRDataField* subData in data.components) {
-                [subArray addObject: (NSDictionary*)[self rtrDataFieldObject: subData]];
-            }
-            
-            if (subArray.count > 0)
-            [object setObject: subArray forKey:@"components"];
-        }
-
-        [array addObject: object];
-    }
-    
-    return array;
+- (NSDictionary*) dictionaryFromDataField: (RTRDataField*) field
+{
+    return @{
+        @"id" : field.id ?: @"",
+        @"name" : field.name ?: @"",
+        @"text" : field.text ?: @"",
+        @"quadrangle" : [self quadrangleString: field.quadrangle],
+        @"components" :[self arrayFromDataFields: field.components],
+    };
 }
 
--(NSMutableDictionary*) rtrDataFieldObject: (RTRDataField*) field {
-    NSMutableDictionary* dictionary = [NSMutableDictionary new];
-    
-    if (field.name.length != 0)
-    [dictionary setObject: field.name forKey: @"name"];
-    
-    if (field.id.length != 0)
-    [dictionary setObject: field.id forKey: @"id"];
-    
-    if (field.text.length != 0)
-    [dictionary setObject: field.text forKey: @"text"];
-    
-    NSString* quadrangleString = [self quadrangleString: field.quadrangle];
-    
-    if (quadrangleString.length != 0)
-    [dictionary setObject: quadrangleString forKey: @"quadrangle"];
+- (NSArray*) arrayFromDataFields: (NSArray<RTRDataField*>*) dataFields {
+    if(dataFields.count == 0) {
+        return @[];
+    }
 
-    return dictionary;
+    NSMutableArray* result = [NSMutableArray arrayWithCapacity:dataFields.count];
+    for(RTRDataField* field in dataFields) {
+        [result addObject: [self dictionaryFromDataField: field]];
+    }
+
+    return result;
 }
 
 -(NSString*) quadrangleString: (NSArray<NSValue*>*) quadrangle {
